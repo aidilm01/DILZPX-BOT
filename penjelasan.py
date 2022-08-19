be4782bc0dafa0355ae965ba02f2b9ec717573ef
aidilm01 <110966734+aidilm01@users.noreply.github.com>
2022-08-19 20:05:46 +0700

    Add files via upload

---
+++ penjelasan.py
@@ -0,0 +1,8 @@
+jika kalian keluar dari termux trus scriptnya harus install ulang
+tinggal ketik aja
+
+1 cd (nama file)
+jika file nya terdeteksi tinggal klian start aja dengan cara
+2 npm start
+atsu
+3 node .
